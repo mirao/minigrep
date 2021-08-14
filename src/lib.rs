32c07141c5cb4d30e64ff8987098b600a8a8d93c
Jaromir Obr <jaromir.obr@gmail.com>
2021-08-14 15:08:47 +0200

    Replaced search alghoritm by iterator

--- src/lib.rs
+++ src/lib.rs
@@ -53,25 +53,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -197,8 +190,8 @@ mod tests {
         Trust me.";
 
         assert_eq!(
-            vec!["Rust:", "Trust me."],
-            super::search_case_insensitive(query, contents)
+            super::search_case_insensitive(query, contents),
+            vec!["Rust:", "Trust me."]
         );
     }
 }
